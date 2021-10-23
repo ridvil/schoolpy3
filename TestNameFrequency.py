6b3a4a7a79ec048be592284aeb780df771ba9cde
ridvil <76529387+ridvil@users.noreply.github.com>
2021-10-23 15:01:59 -0400

    Create TestNameFrequency.py

---
+++ TestNameFrequency.py
@@ -0,0 +1,13 @@
+import pytest
+from NameFrequency import NameFrequency
+
+
+@pytest.fixture()
+def name_frequency():
+    name_frequency = NameFrequency()
+    return name_frequency
+
+
+def test_CanReadCsvInDataframe(name_frequency):
+    name_frequency.preparingData('users.csv', 'name')
+    assert name_frequency.preparingData('users.csv', 'name') == True
